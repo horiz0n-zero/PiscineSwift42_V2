33651194fd034c5e0499b33ec802b11028274f4d
Antoine FEUERSTEIN <afeuerst@e3r10p1.42.fr>
2019-03-06 10:44:21 +0100

    day08 - day09 images

--- day09/day09/ViewController.swift
+++ day09/day09/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import day08_framework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+                let f = Article.newArticle()
+        
+                f.content = "toto"
+                f.title = "toto"
+                f.creationDate = NSDate.init()
+                f.image = UIImagePNGRepresentation(UIImage.init(named: "1")!)! as! NSData
+                let u = Article.newArticle()
+        
+                u.content = "titi"
+                u.title = "jhjh"
+                u.creationDate = NSDate.init()
+                u.image = UIImagePNGRepresentation(UIImage.init(named: "2")!)! as! NSData
+                Article.save()
     }
 
 }
