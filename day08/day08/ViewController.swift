af0e4c0d91b8b553941fb74da4645629f999fb96
Antoine FEUERSTEIN <afeuerst@e3r10p1.42.fr>
2019-03-06 10:22:28 +0100

    day08

--- day08/day08/ViewController.swift
+++ day08/day08/ViewController.swift
@@ -23,14 +23,98 @@ class ArticleTableViewCell: UITableViewCell {
     
 }
 
+class AddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var descriptionTextview: UITextView!
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var cameraImage: UIButton!
+    @IBOutlet var photoImage: UIButton!
+    
+    
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var addbutton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for button in [self.cancelButton, self.addbutton, self.cameraImage, self.photoImage, self.titleTextField, self.descriptionTextview, self.imageView] as! [UIView] {
+            button.layer.cornerRadius = 6
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.blue.cgColor
+            button.layer.masksToBounds = true
+            button.clipsToBounds = true
+        }
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func addTapped(_ sender: Any) {
+        guard let title = self.titleTextField.text, title != "", let content = self.descriptionTextview.text, content != "",
+            let image = self.imageView.image else {
+                let alert = UIAlertController.init(title: "Error", message: "content missing", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction.init(title: "Ok", style: .default, handler: nil))
+                return self.present(alert, animated: true, completion: nil)
+        }
+        
+        let article = Article.newArticle()
+        
+        article.content = content
+        article.title = title
+        article.creationDate = NSDate.init()
+        article.image = UIImagePNGRepresentation(image) as NSData?
+        ViewController.shared.articles.append(article)
+        ViewController.shared.articleTableView.reloadSections(IndexSet.init(integersIn: 0...0), with: .automatic)
+        Article.save()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        let picker = UIImagePickerController.init()
+        
+        picker.sourceType = .camera
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    @IBAction func photoTapped(_ sender: Any) {
+        let picker = UIImagePickerController.init()
+        
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            print("Image not found!")
+            return
+        }
+        
+        self.imageView.image = selectedImage
+    }
+    
+}
+
 class ViewController: UIViewController, UITableViewDataSource {
     
     var articles: [Article] = []
     
     @IBOutlet var articleTableView: UITableView!
+    @IBAction func addArticleTapped(sender: UIButton) {
+        if let vc = self.storyboard?.instantiateViewController(withIdentifier: "AddViewController") as? AddViewController {
+            
+            self.present(vc, animated: true, completion: nil)
+        }
+    }
+    static var shared: ViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        ViewController.shared = self
         self.articles = Article.getAllArticles()
         self.articleTableView.dataSource = self
 //        let f = Article.newArticle()
